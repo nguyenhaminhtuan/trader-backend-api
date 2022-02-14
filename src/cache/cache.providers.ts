2e5cc64efe6805708f9b9eeda9b96a189c9514e3
Nguyen Ha Minh Tuan <nguyenhaminhtuan1997@gmail.com>
2022-02-14 17:38:23 +0700

    feat(cache): add log error for redis service

--- src/cache/cache.providers.ts
+++ src/cache/cache.providers.ts
@@ -10,17 +10,15 @@ export const redisServiceProvider: Provider = {
     configService: ConfigService<EnvironmentVariables>
   ): Promise<RedisService> => {
     const logger = new Logger(CacheModule.name)
-    try {
-      const redisService = new RedisService({
-        host: configService.get('REDIS_HOST'),
-        port: +configService.get('REDIS_PORT'),
-      })
-      await redisService.info()
-      logger.log('Redis successfully connected')
+    const redisService = new RedisService({
+      host: configService.get('REDIS_HOST'),
+      port: +configService.get('REDIS_PORT'),
+    })
+    logger.log('Redis successfully connected')
+    redisService.on('error', (err) => {
+      logger.error({err})
+    })
 
-      return redisService
-    } catch (error) {
-      logger.error(error)
-    }
+    return redisService
   },
 }
