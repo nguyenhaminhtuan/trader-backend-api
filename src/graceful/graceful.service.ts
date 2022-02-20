efac5be50c52a21e035ceb6857e64ba97254fc57
Nguyen Ha Minh Tuan <nguyenhaminhtuan1997@gmail.com>
2022-02-20 22:12:18 +0700

    fix(ts): add strict

--- src/graceful/graceful.service.ts
+++ src/graceful/graceful.service.ts
@@ -2,7 +2,7 @@ import {INestApplication, Inject, Injectable, Logger} from '@nestjs/common'
 import {RedisService} from 'redis'
 import {DB_CLIENT} from 'database'
 import {MongoClient} from 'mongodb'
-import closeWithGrace from 'close-with-grace'
+import closeWithGrace, {Signals} from 'close-with-grace'
 
 @Injectable()
 export class GracefulService {
@@ -14,25 +14,28 @@ export class GracefulService {
   ) {}
 
   async use(app: INestApplication, opts: {delay: number}) {
-    closeWithGrace({delay: opts.delay}, async ({err, signal}) => {
-      if (err) {
-        this.logger.error({err})
+    closeWithGrace(
+      {delay: opts.delay},
+      async (opts: {err?: Error; signal?: Signals; manual?: boolean}) => {
+        if (opts.err) {
+          this.logger.error({err: opts.err})
+        }
+
+        if (opts.signal) {
+          this.logger.log(`${opts.signal} received`)
+        }
+
+        this.logger.log('Start shutdown application gracefully')
+
+        this.redisService.disconnect(false)
+        this.logger.log('Redis disconnected')
+
+        await this.dbClient.close()
+        this.logger.log('Database connection closed')
+
+        await app.close()
+        this.logger.log('Application closed')
       }
-
-      if (signal) {
-        this.logger.log(`${signal} received`)
-      }
-
-      this.logger.log('Start shutdown application gracefully')
-
-      this.redisService.disconnect(false)
-      this.logger.log('Redis disconnected')
-
-      await this.dbClient.close()
-      this.logger.log('Database connection closed')
-
-      await app.close()
-      this.logger.log('Application closed')
-    })
+    )
   }
 }
